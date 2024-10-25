a4ac0af4885079052fae597fac556131fda480d3
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-10-24 21:43:27 -0500

    Finish the dice rolling game project

--- dice/Program.cs
+++ dice/Program.cs
@@ -6,7 +6,52 @@ namespace Dice_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to roll the dice.");
+            int playerRandomNum;
+            int playerScore = 0;
+            int computerRandomNum;
+            int computerScore = 0;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+                
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                computerRandomNum = random.Next(1, 7);
+                
+                Console.WriteLine("You rolled a " + playerRandomNum);
+                Console.WriteLine("Computer rolled a " + computerRandomNum);
+
+                if (playerRandomNum > computerRandomNum) 
+                {
+                    playerScore++;
+                }
+                else if (computerRandomNum > playerRandomNum)
+                {
+                    computerScore++;
+                }
+                Console.WriteLine("");
+            }
+            if (playerScore > computerScore)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("You won the most dice rolls, You won the game!");
+            }
+            else if (playerScore < computerScore)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("The computer won the most dice rolls, you lose!");
+            }
+            else 
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Draw!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
