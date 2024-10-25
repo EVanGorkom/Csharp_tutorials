0b9f565526c8620d798078fe8885ea1ca93dcded
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-10-24 21:21:57 -0500

    Update the subdirectories to remove their bin and obj directories, and create the beginning of the 'dice' project

---
+++ dice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dice_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to roll the dice.");
+        }
+    }
+}
