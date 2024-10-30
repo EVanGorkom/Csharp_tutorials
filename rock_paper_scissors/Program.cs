091aeb71196d663b096a66bac96d41d8e0c7f763
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-10-30 00:05:04 -0500

    Update the rock_paper_scissors game for full functionality

--- rock_paper_scissors/Program.cs
+++ rock_paper_scissors/Program.cs
@@ -7,17 +7,16 @@ namespace Rock_Paper_Scissors
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> choices = new Dictionary<string, string>();
-            choices["r"] = "rock";
-            choices["p"] = "paper";
-            choices["s"] = "scissors";
+            Dictionary<int, string> choices = new Dictionary<int, string>();
+            choices[1] = "rock";
+            choices[2] = "paper";
+            choices[3] = "scissors";
 
             Random random = new Random();
-            int random_choice = random.Next(1, 4);
-            int playerWins;
-            int computerWins;
+            int playerWins = 0;
+            int computerWins = 0;
             string playerChoice;
-            
+            string computerChoice;
 
             Console.WriteLine("Welcome to Rock-Paper-Scissors!\nBest of 3 wins!");
             
@@ -27,22 +26,79 @@ namespace Rock_Paper_Scissors
                 Console.WriteLine("What will be your choice for the round?\nPress 'r' for rock, 'p' for paper, or 's' for scissors.");
                 playerChoice = Console.ReadLine();
 
+                int random_choice = random.Next(1, 4);
+                computerChoice = choices[random_choice];
+                Console.WriteLine("The computer chose " + computerChoice);
+
                 if (playerChoice == "r")
                 {
-                    
+                    if (computerChoice == "rock")
+                    {
+                        Console.WriteLine("Draw!");
+                    }
+                    else if (computerChoice == "paper")
+                    {
+                        Console.WriteLine("You lose this round.");
+                        computerWins++;
+                    }
+                    else if (computerChoice == "paper")
+                    {
+                        Console.WriteLine("You won the round!");
+                        playerWins++;
+                    }
                 }
+
                 else if (playerChoice == "p")
                 {
-
+                    if (computerChoice == "paper")
+                    {
+                        Console.WriteLine("Draw!");
+                    }
+                    else if (computerChoice == "scissors")
+                    {
+                        Console.WriteLine("You lose this round.");
+                        computerWins++;
+                    }
+                    else if (computerChoice == "rock")
+                    {
+                        Console.WriteLine("You won the round!");
+                        playerWins++;
+                    }
                 }
+
                 else if (playerChoice == "s")
                 {
-
+                    if (computerChoice == "scissors")
+                    {
+                        Console.WriteLine("Draw!");
+                    }
+                    else if (computerChoice == "rock")
+                    {
+                        Console.WriteLine("You lose this round.");
+                        computerWins++;
+                    }
+                    else if (computerChoice == "paper")
+                    {
+                        Console.WriteLine("You won the round!");
+                        playerWins++;
+                    }
                 }
+
                 else
                 {
                     Console.WriteLine("Please enter a valid option of either 'r', 'p', or 's'.");
                 }
+            }
+
+            if (playerWins >= 2)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("You won the game!");
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Game Over.");
             }
         }
     }
